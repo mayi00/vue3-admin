eb27167b308e9620c8d797e0a703c6cf1b8b1173
马大头 <2463619061@qq.com>
2022-06-25 21:34:10 +0800

    build：添加环境变量

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import 'normalize.css' // css样式重置
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+const env = import.meta.env
+console.log('当前环境==>', env.VITE_NODE_ENV, '环境变量==>', env);
+
 const app = createApp(App)
 
 app.use(ElementPlus)
