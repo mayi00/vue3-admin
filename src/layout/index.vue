31781d167a3414a7231899e137a2a0e7e0aac8a2
马大头 <2463619061@qq.com>
2022-06-25 19:33:14 +0800

    init

---
+++ src/layout/index.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Description : 
+ * @Author      : huazf
+ * @Date        : 2022-06-25
+ * @LastEditors : huazf
+ * @LastEditTime: 2022-06-25
+ * @FilePath    : \vue3-vite-pc\src\layout\index.vue
+-->
+<template>
+  
+</template>
---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="home">home</p>
+  {{ count }}
+  <el-button class="home" @click="count++">+1</el-button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style lang="less" scoped>
+.home {
+  font-size: 32px;
+}
+</style>
