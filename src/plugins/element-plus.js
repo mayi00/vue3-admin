9a636a74054ce8d86c1fd02a1546e612a2bc5bfb
MDT <2463619061@qq.com>
2025-05-13 17:54:21 +0800

    docs(element-plus): 添加文件头注释并引入新的样式文件
    为 `element-plus.js` 文件添加了文件头注释，包含描述、作者、日期等信息。同时引入了 `el-notification.css` 和 `el-loading.css` 样式文件，以支持更多的 Element Plus 组件样式。

--- src/plugins/element-plus.js
+++ src/plugins/element-plus.js
@@ -1,5 +1,17 @@
+/*
+ * @Description :
+ * @Author      : MDT
+ * @Date        : 2025-02-12
+ * @LastEditors : MDT
+ * @LastEditTime: 2025-05-13
+ * @FilePath    : \vue3-admin\src\plugins\element-plus.js
+ * 代码是写出来给人看的，附带能在机器上运行。
+ * Copyright (c) 2025 by MDT, All Rights Reserved.
+ */
 // 暗黑模式的css样式
 import 'element-plus/theme-chalk/dark/css-vars.css'
 // 全局引入 elementplus 的函数式组件的样式
 import 'element-plus/theme-chalk/el-message.css'
 import 'element-plus/theme-chalk/el-message-box.css'
+import 'element-plus/theme-chalk/el-notification.css'
+import 'element-plus/theme-chalk/el-loading.css'
