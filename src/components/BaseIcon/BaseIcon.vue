580711bba0228228a0095d521974171d4cf5f111
MDT <2463619061@qq.com>
2025-05-21 17:59:50 +0800

    feat: 添加Income页面和BaseIcon组件
    新增Income页面和BaseIcon组件，用于展示收入信息和处理图标显示逻辑。BaseIcon组件支持多种图标类型，包括Iconify、SVG和图片，并提供了加载和错误处理功能。

---
+++ src/components/BaseIcon/BaseIcon.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+import { UseImage } from '@vueuse/components'
+
+defineOptions({ name: 'BaseIcon' })
+
+const props = defineProps({
+  name: { type: String, required: true },
+  class: { type: [String, Array, Object], default: '' },
+  size: { type: [String, Number], default: '1em' },
+})
+
+const iconStyle = computed(() => ({
+  width: typeof props.size === 'number' ? `${props.size}px` : props.size,
+  height: typeof props.size === 'number' ? `${props.size}px` : props.size,
+}))
+
+const outputType = computed(() => {
+  const hasPathFeatures = str => {
+    return /^\.{1,2}\//.test(str) || str.startsWith('/') || str.includes('/')
+  }
+
+  if (/^https?:\/\//.test(props.name) || hasPathFeatures(props.name) || !props.name) {
+    return 'img'
+  } else if (props.name.includes(':')) {
+    return 'iconify'
+  } else {
+    return 'svg'
+  }
+})
+</script>
+
+<template>
+  <em :class="['base-icon', props.class]" :style="iconStyle">
+    <Icon v-if="outputType === 'iconify'" :icon="name" class="base-icon__iconify" />
+    <svg v-else-if="outputType === 'svg'" class="base-icon__svg" aria-hidden="true">
+      <use :xlink:href="`#icon-${name}`" />
+    </svg>
+    <UseImage v-else-if="outputType === 'img'" :src="name" class="base-icon__image">
+      <template #loading>
+        <i class="base-icon__loading" />
+      </template>
+      <template #error>
+        <i class="base-icon__error" />
+      </template>
+    </UseImage>
+  </em>
+</template>
+
+<style scoped>
+.base-icon {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  fill: currentColor;
+  line-height: 1em;
+}
+
+.base-icon__iconify,
+.base-icon__svg,
+.base-icon__image {
+  flex-shrink: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.base-icon__loading {
+  /* 自定义加载动画样式 */
+  animation: rotate 1s linear infinite;
+  width: 100%;
+  height: 100%;
+}
+
+.base-icon__error {
+  /* 自定义错误图标样式 */
+  background: #ff4d4f;
+  width: 100%;
+  height: 100%;
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ src/views/Demo/Income/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineOptions({ name: 'Income' })
+
+</script>
+
+<template>
+  <div class="g-container">
+    <el-card>
+
+    </el-card>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
